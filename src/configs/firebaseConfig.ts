0526c0ee5718fc5bb66c43a5977fa3fee1aa1835
Bogdan Salionov <salionovbogdan@gmail.com>
2022-04-04 16:17:22 +0300

    wise man once said: it's bad idea

---
+++ src/configs/firebaseConfig.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyAW33CS_BKPac5MxMJoEh135yg2Zv7rnq0",
+    authDomain: "notes-test-84013.firebaseapp.com",
+    databaseURL: "https://notes-test-84013-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "notes-test-84013",
+    storageBucket: "notes-test-84013.appspot.com",
+    messagingSenderId: "459243464822",
+    appId: "1:459243464822:web:7110fdbd2f0a95299d9757"
+};  
